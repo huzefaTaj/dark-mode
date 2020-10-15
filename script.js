bbf8af03c0ccd57ffed96c1901396d8df0b54182
Huzefa Taj <47708830+huzefaTaj@users.noreply.github.com>
2020-10-15 11:29:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var on = document.getElementById('text')
+var off = document.getElementById('text')
+function onn() {
+    on.innerHTML = "Dark Mode On";
+
+}
+function offf() {
+    off.innerHTML = "Dark Mode Off";
+}
